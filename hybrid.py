d8bfb5c9faac867d49831c210e784b1dba5eb849
Mark Rubakh <mrubakh@gmail.com>
2019-12-04 21:35:32 +0100

    hello test

--- hybrid.py
+++ hybrid.py
@@ -7,7 +7,7 @@ from Crypto.Hash import SHA256
 from Crypto.Util import Padding
 from Crypto import Random
 
-
+print('hello')
 def save_publickey(pubkey, pubkeyfile):
     with open(pubkeyfile, 'wb') as f:
         f.write(pubkey.export_key(format='PEM'))
@@ -23,9 +23,9 @@ def load_publickey(pubkeyfile):
 
 def save_keypair(keypair, privkeyfile):
     #The key pair contains the private key, so we want to save it protected with a passphrase
-    #We use the getpass() function of the getpass class to input the passphrase from the user 
+    #We use the getpass() function of the getpass class to input the passphrase from the user
     passphrase = getpass.getpass('Enter a passphrase to protect the saved private key: ')
- 
+
     with open(privkeyfile, 'wb') as f:
         f.write(keypair.export_key(format='PEM', passphrase = passphrase))
 
@@ -37,7 +37,7 @@ def load_keypair(privkeyfile):
         keypairstr = f.read()
     try:
         return RSA.import_key(keypairstr, passphrase = passphrase)
-        
+
     except ValueError:
         print('Error: Cannot import private key from file ' + privkeyfile)
         sys.exit(1)
@@ -81,9 +81,9 @@ for opt, arg in opts:
     elif opt in ('-k', '--kpg'):
         operation = 'kpg'
     elif opt in ('-e', '--enc'):
-        operation = 'enc'    
+        operation = 'enc'
     elif opt in ('-d', '--dec'):
-        operation = 'dec'    
+        operation = 'dec'
     elif opt in ('-p', '--pubkeyfile'):
         pubkeyfile = arg
     elif opt in ('-s', '--privkeyfile'):
@@ -99,7 +99,7 @@ if operation not in ('kpg', 'enc', 'dec'):
     print('Error: Operation must be -k (for key pair generation) or -e (for encryption) or -d (for decryption).')
     sys.exit(1)
 
-#Handle a missing public key file... 
+#Handle a missing public key file...
 #Print an error if pubkeyfile is empty and the operation is 'enc' or 'kpg'
 if (not pubkeyfile) and (operation == 'enc' or operation == 'kpg'):
     print('Error: Name of the public key file is missing.')
@@ -117,14 +117,14 @@ if operation in ('enc', 'dec') and (not outputfile):
     print('Error: Name of output file is missing.')
     sys.exit(1)
 
-if (operation == 'enc') and privkeyfile: 
+if (operation == 'enc') and privkeyfile:
     sign = True
 
 # -------------------
 # key pair generation
 # -------------------
 
-if operation == 'kpg': 
+if operation == 'kpg':
     print('Generating a new 2048-bit RSA key pair...')
     keypair = RSA.generate(2048)
 
@@ -138,17 +138,17 @@ if operation == 'kpg':
 # encryption
 # ----------
 
-elif operation == 'enc': 
+elif operation == 'enc':
     print('Encrypting...')
 
     pubkey = load_publickey(pubkeyfile)
     RSAcipher = PKCS1_OAEP.new(pubkey)
 
     #Read the plaintext from the input file
-    with open(inputfile, 'rb') as f: 
+    with open(inputfile, 'rb') as f:
         plaintext = f.read()
     padded_plaintext = Padding.pad(plaintext, AES.block_size)
-	
+
     symkey = Random.get_random_bytes(32) # we need a 256-bit (32-byte) AES key
     iv = Random.get_random_bytes(AES.block_size)
     AEScipher = AES.new(key, AES.MODE_CBC, iv)
@@ -157,7 +157,7 @@ elif operation == 'enc':
 
     encsymkey = RSAcipher.encrypt(symkey)
 
-    #Compute signature if needed 
+    #Compute signature if needed
     if sign:
         keypair = load_keypair(privkeyfile)
         signer = pss.new(keypair)
@@ -165,7 +165,7 @@ elif operation == 'enc':
         hashfn.update(encsymkey+iv+ciphertext)
         signature = signer.sign(hashfn)
 
-    #Write out the encrypted AES key, the IV, the ciphertext, 
+    #Write out the encrypted AES key, the IV, the ciphertext,
     #and the signature in base64 encoding
     with open(outputfile, 'wb') as f:
         f.write(newline(b'--- ENCRYPTED AES KEY ---'))
@@ -192,7 +192,7 @@ elif operation == 'dec':
     iv = b''
     ciphertext = b''
 
-    with open(inputfile, 'rb') as f:        
+    with open(inputfile, 'rb') as f:
         sep = f.readline()
         while sep:
             data = f.readline()
@@ -225,18 +225,18 @@ elif operation == 'dec':
             print('Error: Public key file is missing, signature cannot be verified.')
         else:
             try:
-                pubkey = 
+                pubkey =
                 pubkey = RSA.import_key(pubkeystr)
                 verifier = PKCS1_PSS.new(pubkey)
                 h = SHA256.new()
-                
+
             except (ValueError, TypeError):
                 yn = input('Do you want to continue nevertheless (y/n)? ')
-                if yn != 'y': 
+                if yn != 'y':
                     sys.exit(1)
-	
+
     #Write out the plaintext into the output file
     with open(outputfile, 'wb') as f:
         f.write(plaintext)
-	
+
     print('Done.')
