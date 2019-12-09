997433208bf73db1b535184e323ba3f1be0b5695
Ankit Sanghi <ankitsanghi@gmail.com>
2019-12-09 18:19:23 +0100

    added checks if the flags are correct

--- server.py
+++ server.py
@@ -183,7 +183,9 @@ def process_message(msg_type, msg, sessionkey=None):
             sessionkey = decrypt_sessionkey(enc_sessionkey)
             msg_length += len(enc_sessionkey)
             # failure to decrypt session key returns in NULL response code
-            if (not sessionkey): return None, BAD_AUTH_AND_DEC, payload
+            if (not sessionkey): 
+                sessionkey = generate_sessionkey()
+                return sessionkey, BAD_AUTH_AND_DEC, None
 
         header_dict = msg_dict['header']
         digit_1 = int(header_dict['version'])
@@ -234,7 +236,7 @@ def decrypt_sessionkey(enc_sessionkey):
 
     except (ValueError, TypeError):
         print("Login message error: cannot decrypt session key.")
-        return None # if failure, return NULL
+        return None # if failure, return Null
 
 
 # verify timestamp_length
@@ -313,24 +315,30 @@ while True:
                     command = command_arguments[0]
 
                 if command == 'MKD':
-                    foldername = f"{CURRENT_DIR}/{command_arguments[2]}"
-                    try:
-                        os.mkdir(foldername)
-                    except OSError:
-                        print(f'The folder \"{foldername}\" could not be created.')
+                    if command_arguments[1] != '-n':
+                        print('An incorrect flag was used. Please use the correct flag.')
                     else:
-                        response = SUCCESS
-                        print(f'The folder \"{foldername}\" has been created.')
+                        foldername = f"{CURRENT_DIR}/{command_arguments[2]}"
+                        try:
+                            os.mkdir(foldername)
+                        except OSError:
+                            print(f'The folder \"{foldername}\" could not be created.')
+                        else:
+                            response = SUCCESS
+                            print(f'The folder \"{foldername}\" has been created.')
 
                 elif command == 'RMD':
-                    foldername = f"{CURRENT_DIR}{command_arguments[2]}"
-                    try:
-                        os.rmdir(foldername)
-                    except OSError:
-                        print(f'The folder \"{foldername}\" could not be removed.')
+                    if command_arguments[1] != '-n':
+                        print('An incorrect flag was used. Please use the correct flag.')
                     else:
-                        response = SUCCESS
-                        print(f'The folder \"{foldername}\" has been removed.')
+                        foldername = f"{CURRENT_DIR}{command_arguments[2]}"                    
+                        try:
+                            os.rmdir(foldername)
+                        except OSError:
+                            print(f'The folder \"{foldername}\" could not be removed.')
+                        else:
+                            response = SUCCESS
+                            print(f'The folder \"{foldername}\" has been removed.')
 
                 elif command == 'GWD':
                     try:
@@ -342,14 +350,18 @@ while True:
                         print(f'The current folder is \"{foldername}\".')
 
                 elif command == 'CWD':
-                    path_of_folder = command_arguments[2]
-                    try:
-                        os.chdir(path_of_folder)
-                    except OSError:
-                        print('The current folder could not be changed via the given path.')
+                    if command_arguments[1] != '-p':
+                        print('An incorrect flag was used. Please use the correct flag.')
                     else:
-                        response_code = SUCCESS
-                        print(f'The current folder is now \"{foldername}\".')
+                        path_of_folder = command_arguments[2]
+                        try:
+                            os.chdir(path_of_folder)
+                        except OSError:
+                            print('The current folder could not be changed via the given path.')
+                        else:
+                            CURRENT_DIR = path_of_folder
+                            response_code = SUCCESS
+                            print(f'The current folder is now \"{foldername}\".')
 
                 elif command == 'LST':
                     try:
@@ -366,37 +378,46 @@ while True:
                         print(f'Successfully sent the list of items in {CURRENT_DIR} to client')
 
                 elif command == 'UPL':
-                    filepath = command_arguments[2]
-                    try:
-                        shutil.copyfile(filepath, CURRENT_DIR)
-                    except OSError:
-                        print(f'The file from \"{filepath}\" could not be uploaded')
+                    if command_arguments[1] != '-f':
+                        print('An incorrect flag was used. Please use the correct flag.')
                     else:
-                        response = SUCCESS
-                        print(f'The file from \"{filepath}\" has been uploaded.')
+                        filepath = command_arguments[2]    
+                        try:
+                            shutil.copyfile(filepath, CURRENT_DIR)
+                        except OSError:
+                            print(f'The file from \"{filepath}\" could not be uploaded')
+                        else:
+                            response = SUCCESS
+                            print(f'The file from \"{filepath}\" has been uploaded.')
 
                 elif command == 'DNL':
-                    filename = command_arguments[2]
-                    dstpath = command_arguments[4]
-                    try:
-                        shutil.copyfile(CURRENT_DIR + name_of_file, destination_path)
-                    except OSError:
-                        print(f'The file \"{filename}\" from \"{dstpath}\" could not be downloaded.')
+                    if command_arguments[1] != '-f' or command_arguments[3] != '-d':
+                        print('An incorrect flag was used. Please use the correct flag.')
                     else:
-                        response = SUCCESS
-                        print(f'The file \"{filename}\" from \"{dstpath}\" has been downloaded.')
+                        filename = command_arguments[2]
+                        dstpath = command_arguments[4]
+                        try:
+                            shutil.copyfile(CURRENT_DIR + filename, dstpath)
+                        except OSError:
+                            print(f'The file \"{filename}\" from \"{dstpath}\" could not be downloaded.')
+                        else:
+                            response = SUCCESS
+                            print(f'The file \"{filename}\" from \"{dstpath}\" has been downloaded.')
 
 
                 elif command == 'RMF':
-                    filename = command_arguments[2]
-                    filepath = CURRENT_DIR + name_of_file
-                    try:
-                        os.remove(filepath)
-                    except OSError:
-                        print(f'The file \"{filename}\" could not be removed.')
+                    if command_arguments[1] != '-f':
+                        print('An incorrect flag was used. Please use the correct flag.')
                     else:
-                        response = SUCCESS
-                        print(f'There file \"{filename}\" has been removed.')
+                        filename = command_arguments[2]
+                        filepath = CURRENT_DIR + filename
+                        try:
+                            os.remove(filepath)
+                        except OSError:
+                            print(f'The file \"{filename}\" could not be removed.')
+                        else:
+                            response = SUCCESS
+                            print(f'There file \"{filename}\" has been removed.')
 
                 send_response(CLIENT_ADDR, TYPE_COMMAND, SESSION_KEY, response.encode('utf-8'))
 
